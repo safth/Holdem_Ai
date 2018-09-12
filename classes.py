8261fb57c85715ef6f64efd090295c2d336aec37
Simon Boivin <simonboivin@Simons-MacBook-Pro.local>
2018-09-12 11:46:54 -0400

    caca 3 joueurs

--- classes.py
+++ classes.py
@@ -63,6 +63,7 @@ class Player():
         self.action = 'f'
         self.done = False
         self.fold = False
+        self.check = False
         self.bet = 0
         self.cpu = cpu
 
@@ -78,12 +79,18 @@ class Player():
     def Get_action(self,bet,min_bet):
         if not self.cpu:
             while True:
-
-                val = input('fold = f, call = c, bet = b ... , check = h...')
-                if val not in ['f','b','c','h']: # TODO mettre le check.
-                    print ("Invalid input")
+                if self.check:#si le bet c'est le min
+                    val = input('fold = f, call = c, bet = b ... , check = h...')
+                    if val not in ['f','b','c','h']: # TODO mettre le check.
+                        print ("Invalid input")
+                    else:
+                        break
                 else:
-                    break
+                    val = input('fold = f, call = c, bet = b ... ')
+                    if val not in ['f','b','c']: # TODO mettre le check.
+                        print ("Invalid input")
+                    else:
+                        break
         #if its a computer that play
         else:
             list = ['f','c','b','h']
@@ -92,11 +99,14 @@ class Player():
         if val == 'f':
             self.done = True
             self.fold = True
+            self.hand = []
         elif val == 'b':
             bet = bet+min_bet
         elif val == 'c':
-            bet=bet
-        self.money = self.money - (bet - self.bet)
+            bet = bet - self.bet
+        elif val == 'h':
+            bet = 0
+        self.money = self.money - bet
         self.bet = bet
         return val, bet
 
@@ -105,88 +115,93 @@ class Player():
 
 
     def check_score(self):
-        card = self.hand + self.board
-        rank_list = [ card['rank'] for card in card]
-        suit_list = [ card['suit'] for card in card]
-        #check for duplicated cards
-        counter_rank = Counter(rank_list)
-        for key in counter_rank:
-            #TODO mettre le highest rank de la carte apres
-            #la pair. (si j'ai une pair d'As, la highest c'Est pas un As)
-            if counter_rank[key] == 2:
-                self.doublet += 1
-                if self.doublet_rank < key:
-                    self.doublet_rank = key
+        # the score is 0 if you fold.
+        if self.fold == False:
+            card = self.hand + self.board
+            rank_list = [ card['rank'] for card in card]
+            suit_list = [ card['suit'] for card in card]
+            #check for duplicated cards
+            counter_rank = Counter(rank_list)
+            for key in counter_rank:
+                #TODO mettre le highest rank de la carte apres
+                #la pair. (si j'ai une pair d'As, la highest c'Est pas un As)
+                if counter_rank[key] == 2:
+                    self.doublet += 1
+                    if self.doublet_rank < key:
+                        self.doublet_rank = key
+                        new_list = rank_list
+                        for i in range(2):rank_list.remove(key)
+                        self.best_rank = max(new_list)
+                        # TODO avoir la deuxime meilleurs pair en compte
+                elif counter_rank[key] == 3:
+                    self.triplet += 1
+                    if self.triplet_rank < key:
+                        self.triplet_rank = key
+                        new_list = rank_list
+                        for i in range(3):rank_list.remove(key)
+                        self.best_rank = max(new_list)
+                elif counter_rank[key] == 4:
+                    self.quadruplet += 1
+                    self.quadruplet_rank = key
                     new_list = rank_list
-                    for i in range(2):rank_list.remove(key)
+                    for i in range(4):rank_list.remove(key)
                     self.best_rank = max(new_list)
-                    # TODO avoir la deuxime meilleurs pair en compte
-            elif counter_rank[key] == 3:
-                self.triplet += 1
-                if self.triplet_rank < key:
-                    self.triplet_rank = key
-                    new_list = rank_list
-                    for i in range(3):rank_list.remove(key)
-                    self.best_rank = max(new_list)
-            elif counter_rank[key] == 4:
-                self.quadruplet += 1
-                self.quadruplet_rank = key
-                new_list = rank_list
-                for i in range(4):rank_list.remove(key)
-                self.best_rank = max(new_list)
 
-        #check for flush (5 card of same suit)
-        counter_suit = Counter(suit_list)
-        for key in counter_suit:
-            if counter_suit[key] == 5:
-                self.flush = 1
-                # TODO faire le flush_rank, si c'est égal.
+            #check for flush (5 card of same suit)
+            counter_suit = Counter(suit_list)
+            for key in counter_suit:
+                if counter_suit[key] == 5:
+                    self.flush = 1
+                    # TODO faire le flush_rank, si c'est égal.
 
-        #check for higest cards
-        self.best_rank = max(rank_list)
-        #check for straight (5 card in order)
-        for icard in card:
-            if (icard['rank']+1 in rank_list and icard['rank']+2 in rank_list and icard['rank']+3 in rank_list and icard['rank']+4 in rank_list):
-                val = icard['rank']
-                self.straight = 1
-                self.straight_rank = icard['rank']+4
-                #check for straigth flush
-                suit_in_straight = [suit_list[rank_list.index(val+i)] for i in range(5) ]
-                if(len(set(suit_in_straight))==1):
-                    self.straightflush = 1
-                #TODO check la flush de As à 5
-                #(14 in rank_list and 2 in rank_list and 3 in rank_list and 4 in rank_list and 5 in rank_list)
-            # straigth royale
-            if 10 in rank_list and 11 in rank_list and 12 in rank_list and 13 in rank_list and 14 in rank_list :
-                val = icard['rank']
-                self.straight = 1
-                #check for straigth flush
-                suit_in_straight = [suit_list[rank_list.index(val+i)] for i in range(5) ]
-                if(len(set(suit_in_straight))==1):
-                    self.Rflush = 1
-        #print(rank_list)
-        #print(suit_list)
-        if self.triplet != 0 and self.doublet !=0:
-            self.fullHouse = 1
+            #check for higest cards
+            self.best_rank = max(rank_list)
+            #check for straight (5 card in order)
+            for icard in card:
+                if (icard['rank']+1 in rank_list and icard['rank']+2 in rank_list and icard['rank']+3 in rank_list and icard['rank']+4 in rank_list):
+                    val = icard['rank']
+                    self.straight = 1
+                    self.straight_rank = icard['rank']+4
+                    #check for straigth flush
+                    suit_in_straight = [suit_list[rank_list.index(val+i)] for i in range(5) ]
+                    if(len(set(suit_in_straight))==1):
+                        self.straightflush = 1
+                    #TODO check la flush de As à 5
+                    #(14 in rank_list and 2 in rank_list and 3 in rank_list and 4 in rank_list and 5 in rank_list)
+                # straigth royale
+                if 10 in rank_list and 11 in rank_list and 12 in rank_list and 13 in rank_list and 14 in rank_list :
+                    val = icard['rank']
+                    self.straight = 1
+                    #check for straigth flush
+                    suit_in_straight = [suit_list[rank_list.index(val+i)] for i in range(5) ]
+                    if(len(set(suit_in_straight))==1):
+                        self.Rflush = 1
+            #print(rank_list)
+            #print(suit_list)
+            if self.triplet != 0 and self.doublet !=0:
+                self.fullHouse = 1
 
-        score = [self.Rflush,
-                self.straightflush,
-                self.quadruplet,
-                self.fullHouse,
-                self.flush,
-                self.straight,
-                self.triplet,
-                self.doublet,
-                self.best_rank]
-        rank =  [0,
-                self.flush_rank,
-                self.quadruplet_rank,
-                0,
-                self.flush_rank,
-                self.straight_rank,
-                self.triplet_rank,
-                self.doublet_rank,
-                self.best_rank]
+            score = [self.Rflush,
+                    self.straightflush,
+                    self.quadruplet,
+                    self.fullHouse,
+                    self.flush,
+                    self.straight,
+                    self.triplet,
+                    self.doublet,
+                    self.best_rank]
+            rank =  [0,
+                    self.flush_rank,
+                    self.quadruplet_rank,
+                    0,
+                    self.flush_rank,
+                    self.straight_rank,
+                    self.triplet_rank,
+                    self.doublet_rank,
+                    self.best_rank]
+        else:
+            score = np.zeros(9)
+            rank = np.zeros(9)
 
 
 
--- main.py
+++ main.py
@@ -1,65 +1,35 @@
 from classes import Deck, Player
 import random
 
-
-
-
-
-def player_turn(player,bet,min_bet):
-    #player play only if they have not fold.
-    if (not player.fold)  and (player.bet < bet):
-        player.board = deck.board
-        action, bet = player.Get_action(bet,min_bet)
-    else:
-        #player dont play
-        player.done = True
-    return bet
-
-
-def initialize_round(num_hit):
-    player1.bet=0
-    player1.done=False
-    cpu1.bet=0
-    cpu1.done=False
-    deck.board.extend(deck.hit(num_hit))
-
-def round_turn(bet,min_bet):
-    player1.print_hand() # SHOW CARDS
-    deck.print_board()
-    print("Bet %0.2f: " % bet)
-    bet = player_turn(player1,bet,min_bet)
-    bet = player_turn(cpu1,bet,min_bet)
-    return bet
-
-def isFinish():
-    num_player = [player1,cpu1]
-    count = len(num_player)
-    for player in num_player:
-        if player.fold == True:
-            count -= 1
-    if count == 0 or count ==1:
-        return True
-    else:
-        return False
-
 def check_winner(pot):
     player1.print_hand() # SHOW CARDS
     cpu1.print_hand()
     deck.print_board()
     score1, rank1 = player1.check_score()
     score2, rank2 = cpu1.check_score()
+    score3, rank3 = cpu2.check_score()
     print(score1)
     print(score2)
+    print(score3)
     for i in range(len(score1)):
-        if score1[i]>score2[i]:
+
+        if (score1[i] > score2[i]) and (score1[i] > score3[i]):
             print('player1 win')
             player1.money = player1.money+pot
             break
-        elif score2[i]>score1[i]:
+
+        elif (score2[i] > score1[i]) and (score2[i] > score3[i]):
             print('player2 win')
             cpu1.money = cpu1.money+pot
             break
-        if score1[i]==score2[i] and score1[i]>0:
+
+        elif (score3[i] > score1[i]) and (score3[i] > score2[i]):
+            print('player3 win')
+            cpu2.money = cpu2.money+pot
+            break
+
+            #si c'est égal entre 1 et 2
+        if score1[i]==score2[i] and score1[i]>0 and score1[i] > score3[i]:
             if rank1[i] > rank2[i]:
                 print('player1 win')
                 player1.money = player1.money+pot
@@ -82,12 +52,133 @@ def check_winner(pot):
                     cpu1.money = cpu1.money+pot/2
                     player1.money = player1.money+pot/2
                     break
+                    #si c'est égal entre 1 et 3
+            if score1[i]==score3[i] and score1[i]>0 and score1[i] > score2[i]:
+                if rank1[i] > rank3[i]:
+                    print('player1 win')
+                    player1.money = player1.money+pot
+                    break
+                elif rank1[i] < rank3[i]:
+                    print('player3 win')
+                    cpu2.money = cpu2.money+pot
+                    break
+                elif rank1[i] == rank3[i]:
+                    if rank1[8] > rank3[8]:
+                        print('player1 win')
+                        player1.money = player1.money+pot
+                        break
+                    elif rank1[8] < rank3[8]:
+                        print('player3 win')
+                        cpu2.money = cpu2.money+pot
+                        break
+                    else:
+                        print('egal')
+                        cpu2.money = cpu2.money+pot/2
+                        player1.money = player1.money+pot/2
+                        break
+
+                #si c'est égal entre 2 et 3
+        if score2[i]==score3[i] and score2[i]>0 and score2[i] > score1[i]:
+            if rank2[i] > rank3[i]:
+                print('player2 win')
+                cpu1.money = cpu1.money+pot
+                break
+            elif rank2[i] < rank3[i]:
+                print('player3 win')
+                cpu2.money = cpu2.money+pot
+                break
+            elif rank2[i] == rank3[i]:
+                if rank2[8] > rank3[8]:
+                    print('player2 win')
+                    cpu1.money = cpu1.money+pot
+                    break
+                elif rank2[8] < rank3[8]:
+                    print('player3 win')
+                    cpu2.money = cpu2.money+pot
+                    break
+                else:
+                    print('egal')
+                    cpu2.money = cpu2.money+pot/2
+                    cpu1.money = cpu1.money+pot/2
+                    break
+                #TODO faire une triple égalité
     print(pot,'$')
 
-def game():
 
-    i=1
-    num_player = 5
+
+
+def player_turn(player,bet,min_bet,flop=False):
+    #player play only if they have not fold.
+    if bet == 0:
+        player.check = True
+    else:
+        player.check = False
+
+    if (not player.fold)  and ((player.bet < bet) or (player.check==True)):
+        player.board = deck.board
+        bet, actual_bet = player.Get_action(bet,min_bet)
+        player.done = False
+    else:
+        #player dont play
+        player.done = True
+        actual_bet = 0
+    return bet, actual_bet
+
+
+def initialize_round(num_hit):
+    player1.bet=0
+    player1.done=False
+    cpu1.bet=0
+    cpu1.done=False
+    cpu2.bet=0
+    cpu2.done=False
+    deck.board.extend(deck.hit(num_hit))
+
+def round_turn(bet,min_bet,iturn,pot):
+    player1.print_hand() # SHOW CARDS
+    deck.print_board()
+    if iturn == 1:
+        print("Bet %0.2f: " % bet)
+        print('pot :',pot,'$')
+        bet, actual_bet = player_turn(player1,bet,min_bet)
+        pot += actual_bet
+        bet, actual_bet = player_turn(cpu1,bet,min_bet)
+        pot += actual_bet
+        bet, actual_bet = player_turn(cpu2,bet,min_bet)
+        pot += actual_bet
+    if iturn == 2:
+        bet, actual_bet = player_turn(cpu2,bet,min_bet)
+        pot += actual_bet
+        print("Bet %0.2f: " % bet)
+        print('pot :',pot,'$')
+        bet, actual_bet = player_turn(player1,bet,min_bet)
+        pot += actual_bet
+        bet, actual_bet = player_turn(cpu1,bet,min_bet)
+        pot += actual_bet
+    if iturn == 3:
+        bet, actual_bet = player_turn(cpu1,bet,min_bet)
+        pot += actual_bet
+        bet, actual_bet = player_turn(cpu2,bet,min_bet)
+        pot += actual_bet
+        print("Bet %0.2f: " % bet)
+        print('pot :',pot,'$')
+        bet, actual_bet =  player_turn(player1,bet,min_bet)
+        pot += actual_bet
+
+    return bet, pot
+
+def isFinish():
+    num_player = [player1,cpu1,cpu2]
+    count = len(num_player)
+    for player in num_player:
+        if player.fold == True:
+            count -= 1
+    if count == 0 or count ==1:
+        return True
+    else:
+        return False
+
+def game(iturn):
     while not isFinish():
         #hit card to each player.
         player1.hand = deck.hit(2)
@@ -96,47 +187,37 @@ def game():
         #cpu3.hand = deck.hit(2)
         #cpu4.hand = deck.hit(2)
         min_bet = 0.25 # 1$ min de bet
-        pot=0
+        pot = 0
+        bet = 0
         print("your money : %0.1f " %player1.money)
-        bet = min_bet
         print(pot,'$')
         #Bet on their hands, prior to
         while not isFinish():
-            bet = round_turn(bet,min_bet)
-            pot += bet
-            print(pot,'$')
+            bet, pot = round_turn(bet,min_bet,iturn,pot)
             #flop
-            if (player1.done == True) and ( cpu1.done == True) :
+            if (player1.done == True) and ( cpu1.done == True) and ( cpu2.done == True) :
                 initialize_round(num_hit=3)
                 while not isFinish():
-                    bet=min_bet
-                    bet = round_turn(bet,min_bet)
-                    pot += bet
-                    print(pot,'$')
+                    bet = 0
+                    bet, pot = round_turn(bet,min_bet,iturn,pot)
                     #the turn
-                    if (player1.done == True) and ( cpu1.done == True):
+                    if (player1.done == True) and ( cpu1.done == True) and ( cpu2.done == True):
                         initialize_round(num_hit=1)
                         min_bet=2*min_bet
                         bet = min_bet
                         while not isFinish():
-                            bet = round_turn(bet,min_bet)
-                            pot += bet
-                            print(pot,'$')
+                            bet, pot = round_turn(bet,min_bet,iturn,pot)
                             #the River
-                            if (player1.done == True) and ( cpu1.done == True):
+                            if (player1.done == True) and ( cpu1.done == True) and ( cpu2.done == True):
                                 initialize_round(num_hit=1)
                                 #min_bet=2*min_bet
                                 while not isFinish():
-                                    bet = round_turn(bet,min_bet)
-                                    pot += bet
-                                    print(pot,'$')
-                                    if (player1.done == True) and ( cpu1.done == True):
+                                    bet, pot = round_turn(bet,min_bet,iturn,pot)
+                                    if (player1.done == True) and ( cpu1.done == True) and ( cpu2.done == True):
                                         check_winner(pot)
                                         return
     print("lui qui a pas fold gagne") # celui qui a pas fold
-
-
-
+    # TODO faire que tu perd quanfd tu fold.
 
 
 
@@ -144,14 +225,21 @@ def game():
 deck = Deck()
 player1 = Player(money=10,name='simon')
 cpu1 = Player(money = 10, cpu = True, name='cpu')
-#cpu2 = Player(money = 10, cpu = True, name='cpu')
+cpu2 = Player(money = 10, cpu = True, name='cpu')
 #cpu3 = Player(money = 10, cpu = True, name='cpu')
 #cpu4 = Player(money = 10, cpu = True, name='cpu')
+
+i=1 #order of play
 while player1.money > 0 and cpu1.money > 0:
-    game()
+    game(i)
     player1.reset()
     cpu1.reset()
+    cpu2.reset()
     deck.reset()
+    #TODO put it back
+    #i += 1
+    #if i==4 : i=1
+
 
 
 
